ad27ffc5f53dd50794c39dd7a99ca23e1eb051c6
木四水 <78152109+ZMuSiShui@users.noreply.github.com>
2021-06-23 18:12:46 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,6 +3,9 @@ GET_LICENSES_URL = "https://store.steampowered.com/dynamicstore/userdata/"
 
 DELIVERY_AREA = "US"
 GAME_APP_ID = {
-    "1313860": "590605",
-    "1172470": "401407"
+    "1180660": "556302",    # Tell Me Why
+    "1308380": "456670",    # Super Bomberman R Online
+    "1172470": "401407",    # Apex Legends
+    "601510": "158055",     # YuGiOh Duel Links
+    "216150": "21462",      # MapleStory
 }
